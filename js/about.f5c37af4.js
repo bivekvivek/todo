4d366513ceec28fc131cf1551ca468c2d42a71ba
bivekvivek <61555851+bivekvivek@users.noreply.github.com>
2020-06-28 13:24:13 +0545

    checking new colors

---
+++ js/about.f5c37af4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")]),n("button",{staticClass:"btn btn-primary"},[t._v("Hello, I am Bivek")])])}],i=n("2877"),l={},u=Object(i["a"])(l,a,s,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.f5c37af4.js.map
